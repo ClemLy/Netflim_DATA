f9a1a4af44ad232aff87e93c9b6abcae74d38f54
ClemLy <XtrayShow@yahoo.fr>
2026-01-19 14:58:21 +0100

    Ajout de validation pour Films

--- src/routes/movie.routes.js
+++ src/routes/movie.routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import { movieController } from '../controllers/movie.controller.js';
 
+// Middlewares de validation
+import { validate } from '../middlewares/validate.middleware.js';
+import { createMovieSchema } from '../validators/movie.validator.js';
+
 const router = Router();
 
-router.post('/', movieController.create);
+router.post('/', validate(createMovieSchema), movieController.create);
 router.get('/', movieController.findAll);
 router.get('/:id', movieController.findOne);
 
